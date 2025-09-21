1f049c5188256989afcf9aa3a0a10cf114d560c3
AmitAzoulay <amitazoulay1611@gmail.com>
2025-09-21 14:53:30 +0300

    now the communication is over tls

--- src/client.rs
+++ src/client.rs
@@ -1,11 +1,28 @@
-use tonic::transport::{Channel, ClientTlsConfig, Certificate};
+use tonic::{transport::{Channel, Certificate, ClientTlsConfig, Identity}, Request, Status};
 
 use hello::say_client::SayClient;
 use hello::SayRequest;
+use std::fs;
+
+
 mod hello;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let channel = tonic::transport::Channel::from_static("http://[::1]:50051")
+
+    // Load CA certificate
+    let ca_pem = std::fs::read("C:\\Users\\Shuly\\Desktop\\hello\\certs\\ca.pem")?;
+    let client_pem = std::fs::read("C:\\Users\\Shuly\\Desktop\\hello\\certs\\client.pem")?;
+    let client_key = std::fs::read("C:\\Users\\Shuly\\Desktop\\hello\\certs\\client.key")?;
+
+    // Create client TLS configuration
+    let tls = ClientTlsConfig::new()
+        .ca_certificate(Certificate::from_pem(ca_pem))
+        .identity(Identity::from_pem(client_pem, client_key))
+        .domain_name("localhost");
+
+
+    let channel = Channel::from_static("https://[::1]:50051")
+        .tls_config(tls)?
         .connect()
         .await?;
 
--- src/server.rs
+++ src/server.rs
@@ -1,8 +1,9 @@
-use tonic::{transport::{Server, ServerTlsConfig}, Request, Response, Status};
+use tonic::{transport::{Server, Identity, ServerTlsConfig, Certificate}, Request, Response, Status};
 
 mod hello;
 use crate::hello::say_server::{Say, SayServer};
 use crate::hello::{SayRequest, SayResponse};
+use std::fs;
 
 #[derive(Default)]
 pub struct MySay;
@@ -10,16 +11,32 @@ pub struct MySay;
 #[tonic::async_trait]
 impl Say for MySay {
     async fn send(&self, req: Request<SayRequest>) -> Result<Response<SayResponse>, Status> {
-        Ok(Response::new(SayResponse { message: format!("hello {}", req.get_ref().name) }))
+        println!("Got a TLS-secured request from {:?}", req.remote_addr());
+
+        let reply = SayResponse {
+            message: format!("Hello {} from a TLS-enabled server!", req.into_inner().name),
+        };
+        Ok(Response::new(reply))
     }
 }
 
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    let server_pem = std::fs::read("C:\\Users\\Shuly\\Desktop\\hello\\certs\\server.pem")?;
+    let server_key = std::fs::read("C:\\Users\\Shuly\\Desktop\\hello\\certs\\server.key")?;
+    let ca_pem = std::fs::read("C:\\Users\\Shuly\\Desktop\\hello\\certs\\ca.pem")?;
+
+    let tls = ServerTlsConfig::new()
+        .identity(Identity::from_pem(server_pem, server_key))
+        .client_ca_root(Certificate::from_pem(ca_pem));
+
+
     let addr = "[::1]:50051".parse()?;
     println!("Server listening on {addr}");
     Server::builder()
+        .tls_config(tls)?
         .add_service(SayServer::new(MySay::default()))
         .serve(addr)
         .await?;
